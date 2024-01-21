b6a4c2429e63973c7341d9a86d385682a9f47705
Gustavo <gustavo_alves2010@yahoo.com.br>
2024-01-21 17:09:11 -0300

    feat: improve travelling salesman implementation using dynamic programming

--- backend/src/helpers/salesman-travelling.ts
+++ backend/src/helpers/salesman-travelling.ts
@@ -3,21 +3,36 @@ type Matrix = Array<Array<number>>;
 type SalesmanTravellingResult = [number, number[]];
 
 const salesman_travelling = (m: Matrix): SalesmanTravellingResult => {
+  const n = m.length;
+  const cache: Record<string, SalesmanTravellingResult> = {};
+
   const salesman_travelling_helper = (
     current: number,
+    mask: number,
     visited: number[]
   ): SalesmanTravellingResult => {
     visited.push(current);
+    mask = mask | (1 << current);
 
-    if (visited.length === m.length) {
+    if (mask === (1 << n) - 1) {
       return [m[current][0], visited];
+    }
+
+    const key = `${current}-${mask}`;
+
+    if (cache[key] != undefined) {
+      return cache[key];
     }
 
     let total_distance: [number, number[]] = [Infinity, []];
 
-    for (let i = 0; i < m.length; i++) {
-      if (current != i && !visited.includes(i)) {
-        let [distance, path] = salesman_travelling_helper(i, visited.slice());
+    for (let i = 0; i < n; i++) {
+      if (current != i && ((mask >> i) & 1) === 0) {
+        let [distance, path] = salesman_travelling_helper(
+          i,
+          mask,
+          visited.slice()
+        );
         distance += m[current][i];
 
         if (distance < total_distance[0]) {
@@ -26,10 +41,12 @@ const salesman_travelling = (m: Matrix): SalesmanTravellingResult => {
       }
     }
 
+    cache[key] = total_distance;
+
     return total_distance!;
   };
 
-  return salesman_travelling_helper(0, []);
+  return salesman_travelling_helper(0, 1, []);
 };
 
 export { salesman_travelling, Matrix, SalesmanTravellingResult };

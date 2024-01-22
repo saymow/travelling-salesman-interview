da93817f75ea56a6b89b097b21ad57651483fd13
Gustavo <gustavo_alves2010@yahoo.com.br>
2024-01-22 12:30:01 -0300

    test: homescreen - ensure api call use pagination page input

--- frontend/cypress/e2e/home-screen.cy.ts
+++ frontend/cypress/e2e/home-screen.cy.ts
@@ -12,4 +12,38 @@ describe("HomeScreen", () => {
 
     cy.wait("@clients-search");
   });
+
+  it("Should call the api with the correct pagination page values", async () => {
+    cy.visit("http://localhost:5173");
+
+    const goToNextPageBtn = cy.get("[title='Go to next page']");
+    const goToPreviousPageBtn = cy.get("[title='Go to previous page']");
+
+    cy.fixture("list-clients-response.json").then((body) => {
+      cy.intercept(`http://localhost:3333/clients*`, {
+        statusCode: 200,
+        body,
+      });
+    });
+
+    cy.intercept(
+      `http://localhost:3333/clients?limit=${10}&offset=${10}&q[email]=&q[name]=&q[phone]=`
+    ).as("clients-paginate-forward");
+
+    goToNextPageBtn.click();
+
+    cy.wait("@clients-paginate-forward");
+
+    goToNextPageBtn.should("be.disabled");
+
+    cy.intercept(
+      `http://localhost:3333/clients?limit=${10}&offset=${0}&q[email]=&q[name]=&q[phone]=`
+    ).as("clients-paginate-backward");
+
+    goToPreviousPageBtn.click();
+
+    cy.wait("@clients-paginate-backward");
+
+    goToPreviousPageBtn.should("be.disabled");
+  });
 });
